7bcb3ffcfee9a36d3b134b1cb166f3e7f3b0cde1
sbkogs <sbkoganti@gmail.com>
2015-06-08 21:22:33 -0700

    Add way to enter a note

--- html/js/app.js
+++ html/js/app.js
@@ -3,8 +3,32 @@ var m = require('mithril');
 var notelist = {};
 var editnote = {};
 
-editnote.view = function () {
-	return m('input', 'Enter');
+editnote.controller = function () {
+	this.note = m.prop('');
+    this.save = function () {
+        console.log(this.note())
+        this.note('')
+    }.bind(this);
+};
+
+editnote.view = function (ctrl) {
+	return m('.container',
+			m('.row',
+				m('.col-md-6',
+					m('div.form-group',
+						m('textarea.form-control', {
+							oninput: m.withAttr("value", ctrl.note),
+      						value: ctrl.note(),
+    						placeholder:'Add notes',
+							rows: 3
+						})),
+					m('button.btn.btn-default', {
+							type: 'submit',
+                            onclick: ctrl.save
+						}, 'Save')
+				)
+			)
+	);
 };
 
 m.mount(document.getElementById('content'), editnote);
